4084ee0f855717d6ae697f2b04a9cacdfb4f6fa9
Leo Orpilla III <dev@ldgrp.me>
2023-04-03 08:56:00 +1000

    Add resize listener

--- src/scene.ts
+++ src/scene.ts
@@ -5,7 +5,7 @@ import { lightManager } from './lights';
 
 export const scene = new THREE.Scene()
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.useLegacyLights = false;
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.shadowMap.enabled = true;
@@ -23,3 +23,12 @@ export function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+
+function onWindowResize(camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(devicePixelRatio);
+}
+
+window.addEventListener('resize', () => onWindowResize(camera, renderer))
